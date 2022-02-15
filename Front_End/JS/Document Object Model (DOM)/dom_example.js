b15239f5fc071d20f5eaa6b0f15d2d327897f8c7
razalar <razalar@yahoo.com>
2022-02-15 08:42:08 -0500

    Updated DOM folder

---
+++ Front_End/JS/Document Object Model (DOM)/dom_example.js
@@ -0,0 +1,4 @@
+//To view the DOM of a website as a JavaScript object, you need to go to the console
+//and use the following:
+//console.dir(document)
+//This will bring up all the keys of the document object.
