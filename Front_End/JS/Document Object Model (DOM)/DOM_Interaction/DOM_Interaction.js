a1e7ad771d3eb5bbee3b311b2e86dbd9276dcde5
razalar <razalar@yahoo.com>
2022-02-15 09:31:26 -0500

    DOM files

---
+++ Front_End/JS/Document Object Model (DOM)/DOM_Interaction/DOM_Interaction.js
@@ -0,0 +1,17 @@
+//Important attributes
+// document.URL
+// Returns the URL of the website.
+// document.body
+// Returns everything inside the body tags
+// document.head
+// Returns everything in the head of the page
+// document.links
+// Returns all links on the page
+
+//Methods of grabing elements
+
+// document.getElementById('id')
+// document.getElementsByClassName('className')
+// document.getElementsByTagName('tagName')
+// document.querySelector()
+// document.querySelectorAll()
