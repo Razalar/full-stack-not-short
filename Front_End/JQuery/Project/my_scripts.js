a0faee6d147de78cc680d5f561f8a80203316fa0
razalar <razalar@yahoo.com>
2022-02-23 08:08:19 -0500

    v

--- Front_End/JQuery/Project/my_scripts.js
+++ Front_End/JQuery/Project/my_scripts.js
@@ -1,1 +1,40 @@
+//game variables
 var circles = document.querySelectorAll('td');
+var turn = 0;
+var gameOn = true;
+
+//Set player names and colors
+
+var pOneName = prompt("Player One: Enter Your Name, you will be Blue");
+var pTwoName = prompt("Player Two: Enter Your Name, you will be Red");
+
+//check if game is still playing
+while (gameOn) {
+
+  //turn tracking
+  if (turn%2 === 0) {
+    $('.playerName').text(pOneName);
+    console.log('p1 turn');
+
+
+
+
+    turn++;
+  }else {
+    $('.playerName').text(pOneName);
+    console.log('p2 turn');
+
+
+
+
+
+    turn++;
+  }
+}
+
+
+
+
+
+
+//button color change base on player's turn
